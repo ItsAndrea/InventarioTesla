58b52c14f2fed56af9b0d44b05ab08c0058235f5
Marlon Falquez M <mjfalquez@uninorte.edu.co>
2021-10-18 12:03:23 -0500

    Se crea constante URL y se cierra la conexión DB

--- gestorDB.py
+++ gestorDB.py
@@ -1,25 +1,32 @@
 import sqlite3
 
-def ejecutar(query) -> int:
+URL ='static\db\Tesla.db'
+
+def ejecutar(query,params) -> int:
     """ Ejecuta una consulta de acción y retorna el número de registros afectadas """
     try:
-        with sqlite3.connect('static\db\Tesla.db') as conn:
+        with sqlite3.connect(URL) as conn:
             warea = conn.cursor()
-            rst = warea.execute(query).rowcount
+            rst = warea.execute(query,params).rowcount
             if rst!=0:
                 conn.commit()
-            conn.close()
     except Exception as e:
         rst = 0
+    finally:
+            warea.close()
+            conn.close()
     return rst
 
-def seleccionar(query) -> list:
+def seleccionar(query,params) -> list:
     """ Ejecuta una consulta de seleccion y retorna los registros seleccionados """
     try:
-        with sqlite3.connect('static\db\Tesla.db') as conn:
+        with sqlite3.connect(URL) as conn:
             warea = conn.cursor()
-            rst = warea.execute(query).fetchall()
+            rst = warea.execute(query,params).fetchall()
             conn.close()
     except Exception as ex:
         rst = None
+    finally:
+            warea.close()
+            conn.close()
     return rst
