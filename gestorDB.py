a4808d1170fdf82dfd61aa92cb1512ab3ad65791
aaron <aaroncabrales35@gmail.com>
2021-10-20 13:26:31 -0500

    commit a

---
+++ gestorDB.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+URL ='static\db\Tesla.db'
+
+def ejecutar(query,params) -> int:
+    """ Ejecuta una consulta de acción y retorna el número de registros afectadas """
+    try:
+        with sqlite3.connect(URL) as conn:
+            warea = conn.cursor()
+            rst = warea.execute(query,params).rowcount
+            if rst!=0:
+                conn.commit()
+    except Exception as e:
+        rst = 0
+    finally:
+            warea.close()
+            conn.close()
+    return rst
+
+def seleccionar(query,params) -> list:
+    """ Ejecuta una consulta de seleccion y retorna los registros seleccionados """
+    try:
+        with sqlite3.connect(URL) as conn:
+            warea = conn.cursor()
+            rst = warea.execute(query,params).fetchall()
+            conn.close()
+    except Exception as ex:
+        rst = None
+    finally:
+            warea.close()
+            conn.close()
+    return rst
