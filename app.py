8851c8879dc43dd761e00ac67af09284b6db411b
Aaron cabrales <79927983+aaroncabc@users.noreply.github.com>
2021-10-24 12:54:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,16 @@ from flask.sessions import NullSession
 from markupsafe import escape
 from werkzeug.datastructures import TypeConversionDict
 from forms import FAsociar, FcalificarProducto, FUsuario, Login, FProducto, FProveedor 
+#librerios hash
+from hashlib import sha512, md5
+from werkzeug.security import check_password_hash, generate_password_hash
 import os
 
+#funcion provisional
+def trash(q):
+    a=str(q[0]).replace("('"," ")
+    a=a.replace("',)","")
+    return a
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -20,49 +28,44 @@ def home():
         return redirect('/login/')
 
 
-@app.route('/logout/')
-def logout():
-    session.clear()
-    return render_template('flogin.html')
-
+@app.route('/logout/',methods=["GET","POST"])
 @app.route("/login/", methods=["GET","POST"])
 def login():
     frm = Login()
+    session.clear()
     if request.method=='GET':
         return render_template('flogin.html',titulo='Control de acceso', form=frm)
     else:
-        # Recuperar los datos
-        mail = request.form['mail']
-        pwd = request.form['psw']
-        admin= True
-        # validar los datos
-        swvalido = True
-        if len(mail)<6 or len(mail)>40:
-            swvalido = False
-        if len(pwd)<6 or len(pwd)>40:
-            swvalido = False
-        #validar rol
-        if swvalido and mail!="aaroncabrales@gmail.com" and pwd!="1234567":
-            admin=False
-        if mail!="aaroncabrales@gmail.com" and pwd!="1234567":
-            admin=False
-        # Realizar el login simulado
-        if  admin:
-            session.clear()
-            session['logged']='T'
-            session['usr_id'] = mail
-            session['pwd_id'] = pwd
-            session['admin_id']='Administrador'
-            return render_template('home.html',admin_id=session['admin_id'])
-        elif swvalido:
-            session.clear()
-            session['logged']='T'
-            session['usr_id'] = mail
-            session['pwd_id'] = pwd
-            admin_id='Usuario final'
-            return render_template('home.html',admin_id=admin_id)
+         # Recuperar los datos
+        mail = escape(request.form['mail'].strip())
+        pwd = escape(request.form['psw'].strip())
+        admin= False
+        p=False
+        q=f"SELECT clave FROM usuarios WHERE email= '{mail}' "
+        a=gestorDB.seleccionar(q,"")
+        a= trash(a).strip()
+        if check_password_hash(a,pwd):
+            p=True
         else:
-            return render_template('flogin.html',titulo='Control de acceso', form=frm)
+            p=False
+        query1=f"SELECT * FROM usuarios WHERE (email= '{mail}') and (estado='A') "
+        query2=f"SELECT * FROM usuarios WHERE (email= '{mail}') and (rol='Admin') or (rol='Super')"
+        usr=gestorDB.seleccionar(query1,"")
+        rol=gestorDB.seleccionar(query2,"")
+        if len(rol)>0 and p:
+            admin=True
+        if len(usr)>0 and p:
+            session.clear()
+            session['logged']='T'
+            session['usr_id'] = mail
+            session['pwd_id'] = pwd
+            if admin:
+                session['admin_id']='T'
+                return render_template('home.html',admin_id="admin")
+            else:
+                return render_template('home.html',admin_id="usuario")
+        else:
+            return render_template('flogin.html')
 
 
 @app.route("/recuperarContrasena/", methods=['GET','POST'])
